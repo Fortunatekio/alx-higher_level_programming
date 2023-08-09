5e4d5bc5fb80908c698b26603bdf2189b8c9b871
Fortunatekio <fortunatemugendi8@gmail.com>
2023-08-10 02:34:40 +0300

    elseif

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,5 +7,6 @@ def uppercase(str):
      for c in str:
          if ord(c) >= 97 and ord(c) <= 122:
               c = chr(ord(c) - 32)
+              converted_word += c
               print("{}".format(c), end="")
               print("")

e3594ecdb93da38abb36a4ff6d75a51878ddc71c
Fortunatekio <fortunatemugendi8@gmail.com>
2023-09-13 00:00:14 +0300

    writing a script that writes stdin

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,6 +15,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
         If attrs is a list of strings, represents only those attributes
